86660cd65191cede97d085f8382891fa0ddb70b8
JovannyRch <jovannyrch@gmail.com>
2024-01-17 16:40:44 -0600

    Add deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jovannyrch.github.io",
+  base: "/tablas_de_verdad_calculadora",
   integrations: [
     react(),
     tailwind({
